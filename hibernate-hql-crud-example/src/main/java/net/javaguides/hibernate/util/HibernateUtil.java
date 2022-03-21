3224f83cbd0afe43b9376a959a66e3bfcb63fef8
venkatdasari05 <venkateshdasarivasu@gmail.com>
2022-03-21 15:50:49 +0530

    i did some changes in the existing code

--- hibernate-hql-crud-example/src/main/java/net/javaguides/hibernate/util/HibernateUtil.java
+++ hibernate-hql-crud-example/src/main/java/net/javaguides/hibernate/util/HibernateUtil.java
@@ -20,16 +20,16 @@ public class HibernateUtil {
 				// Hibernate settings equivalent to hibernate.cfg.xml's properties
 				Properties settings = new Properties();
 				settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/testdb?useSSL=false");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/testdb?useSSL=true");
 				settings.put(Environment.USER, "root");
 				settings.put(Environment.PASS, "root");
 				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 
-				settings.put(Environment.SHOW_SQL, "true");
+				settings.put(Environment.SHOW_SQL, "false");
 
-				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "fread");
 
-				settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+				settings.put(Environment.HBM2DDL_AUTO, "dont create-drop");
 
 				configuration.setProperties(settings);
 				
